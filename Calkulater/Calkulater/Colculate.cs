b08989088fe7bdc7424ab498dff1e91f84a94e9e
darkness-reveur <uvapuvp@mail.ru>
2020-01-24 23:14:04 +0300

    Colculate

---
+++ Calkulater/Calkulater/Colculate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calkulater
+{
+    class Colculate
+    {
+        //public void Addition(string a, string b)
+        //{
+        //    bool BoolItem1FirstElement = DataProcessing.TypeCheck(a).Item1;
+        //    bool BoolItem2FirstElement = DataProcessing.TypeCheck(a).Item2;
+        //    bool BoolItem1SecondElement = DataProcessing.TypeCheck(b).Item1;
+        //    bool BoolItem2SecondElement = DataProcessing.TypeCheck(b).Item2;
+        //    if (!BoolItem1FirstElement || !BoolItem2FirstElement || !BoolItem1SecondElement || !BoolItem2SecondElement)
+        //    {
+
+        //    }
+        //}
+        public string Addition(string firstElement, string secondElement)
+        {
+           
+            if (!DataProcessing.TypeCheck(firstElement,secondElement))
+            {
+                return firstElement + secondElement;
+            }
+            else
+            {
+                double firstNumber = double.Parse(firstElement);
+                double secondNumber = double.Parse(secondElement);
+                double result = firstNumber + secondNumber;
+                return result.ToString();
+            }
+        }
+        public string Multiplication(string firstElement, string secondElement)
+        {
+            
+            if (DataProcessing.TypeCheck(firstElement, secondElement))
+            {
+                double firstNumber = double.Parse(firstElement);
+                double secondNumber = double.Parse(secondElement);
+                double result = firstNumber * secondNumber;
+                return result.ToString();
+            }
+            else
+            {
+                return "Operator \'*\' cannot be applied to operands of type \'string\' and \'string\'";
+            }
+        }
+        public string Division(string firstElement, string secondElement)
+        {
+            if (DataProcessing.TypeCheck(firstElement, secondElement))
+            {
+                double firstNumber = double.Parse(firstElement);
+                double secondNumber = double.Parse(secondElement);
+                double result = firstNumber / secondNumber;
+                return result.ToString();
+            }
+            else
+            {
+                return "Operator \'/\' cannot be applied to operands of type \'string\' and \'string\'";
+            }
+        }
+        //{
+        //    return a / b;
+        //}
+
+
+        //public string Division(double a, double b)
+        //{
+        //    return a / b;
+        //}
+    }
+}
---
+++ Calkulater/Calkulater/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calkulater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Colculate colculate = new Colculate();
+            bool isFirstElementNegative = false;
+            char operation = ' ';
+            int count = 0;
+            string expression = "";
+            string firstElement = "-";
+            string secondElement = "-";
+            while (true)
+            {
+                Console.WriteLine("Введите выражение");
+                expression = Console.ReadLine().Replace(" ", string.Empty);
+                char[] Elements = expression.ToCharArray();
+                //for (int i=0;i<Elements.Length;i++)
+                //{
+                //    Console.WriteLine(Elements[i]);
+                //}
+                for (int i = 1; i < Elements.Length; i++)
+                {
+                    if (Elements[i] == '/' || Elements[i] == '+' || Elements[i] == '-' || Elements[i] == '*')
+                    {
+                        operation = Elements[i];
+                        count += 1;
+                    }
+                }
+                if (Elements[0] == '-')
+                {
+                    isFirstElementNegative = true;
+                }
+                if (count == 1)
+                {
+                    break;
+                }
+                else if (count < 1)
+                {
+                    Console.WriteLine("Вы не написали операцию для элементов");
+                }
+                else
+                {
+                    Console.WriteLine("На данный момент приложение может производить математические операции только с двумя элементами");
+                }
+                
+            }
+            string[] elements = Spliting(expression);
+            if (isFirstElementNegative)
+            {
+                firstElement += elements[0];
+            }
+            else
+            {
+                firstElement = elements[0];
+            }
+            if (operation == '-')
+            {
+                operation = '+';
+                secondElement += elements[1];
+            }
+            else
+            {
+                secondElement = elements[1];
+            }
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine( " ="+colculate.Addition(firstElement,secondElement));
+                    break;
+                case '/':
+                    Console.WriteLine( " ="+colculate.Division(firstElement,secondElement));
+                    break;
+                case '*':
+                    Console.WriteLine(  " =" +colculate.Multiplication(firstElement,secondElement));
+                    break;
+            }
+
+        }
+        static string[] Spliting(string expression)
+        {
+            return expression.Split(new char[] { '/', '*', '+', '-' });
+        }
+
+    }
+}
